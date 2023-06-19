01a6222c5bd7efb0463b41523cbabe8a1c7bc6a5
Diegoooss <137117142+Diegoooss@users.noreply.github.com>
2023-06-19 18:53:03 -0400

    Add files via upload

---
+++ DIEGOALVAREZ_PGY1121_9D_DIURNO.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def datos_vehiculo():
+   
+        tipo=input("ingrese tipo de vehiulo")
+        patente=input("ingrese patente")
+        marca=input("ingrese marca de vehiculo")
+        precio=int(input("ingrese precio"))
+        fecha=int(input("ingrese fecha"))
+
+def imprimircertificado():
+    print("imprimir ccertificado")
+    print("1. emanacion de aire ")
+        
+      
+
+sw=0
+while sw!=1:
+    print("1. Grabar")
+    print("2. Buscar")
+    print("3. Imprimir certificados")
+    print("4. salir ")
+    opcion=int(input("Seleccione su opcion: "))
+    if opcion ==1:
+        datos_vehiculo()
+    else:
+        if opcion==2:
+            print ("buscar")
+        else:
+            if opcion==3:
+                imprimircertificado
+                
+            else:
+                if opcion==4:
+                    sw=1
+             
+      
+                                                                                                                         
+
+        
